d75e1ae9f782159bc584a7fcae31b2365afebcfb
Iliyan31 <76901712+Iliyan31@users.noreply.github.com>
2023-11-14 09:53:10 +0200

    asdf

--- sql/V0__create_email.sql
+++ sql/V0__create_email.sql
@@ -1,5 +1,5 @@
 create table email (
-    email_id INT(255) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    email_id int auto_increment primary key,
     email_address varchar(100),
     email_type char(1) not null,
     person_id int not null
