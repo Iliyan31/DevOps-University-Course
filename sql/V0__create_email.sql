77817f4029848aef43019edc6cca692572c698da
Iliyan31 <76901712+Iliyan31@users.noreply.github.com>
2023-11-14 09:50:30 +0200

    Change email id

--- sql/V0__create_email.sql
+++ sql/V0__create_email.sql
@@ -1,5 +1,5 @@
 create table email (
-    email_id int not null auto_increment primary key,
+    email_id INT(255) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     email_address varchar(100),
     email_type char(1) not null,
     person_id int not null
