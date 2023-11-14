11053f8f8f791731cf5bba6fadf2cc1fa9bc7c75
Iliyan31 <76901712+Iliyan31@users.noreply.github.com>
2023-11-14 12:16:00 +0200

    Update first migration

--- sql/V0__create_email.sql
+++ sql/V0__create_email.sql
@@ -1,5 +1,5 @@
 create table email (
-    email_id int auto_increment primary key,
+    email_id int serial primary key,
     email_address varchar(100),
     email_type char(1) not null,
     person_id int not null
