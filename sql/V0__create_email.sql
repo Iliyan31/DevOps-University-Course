477e42b6274767feffbe47217cf7bd0a7f907cd1
Iliyan31 <76901712+Iliyan31@users.noreply.github.com>
2023-11-14 12:18:11 +0200

    test

--- sql/V0__create_email.sql
+++ sql/V0__create_email.sql
@@ -1,5 +1,5 @@
 create table email (
-    email_id int serial primary key,
+    email_id serial primary key,
     email_address varchar(100),
     email_type char(1) not null,
     person_id int not null
